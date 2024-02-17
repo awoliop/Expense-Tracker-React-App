096b80c1c158e650b586f4688b2990bfb618d54c
awoliop <awoliioop@gmail.com>
2024-02-17 12:07:48 +0300

    Adding Event listners of all kinds of functions

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,6 +3,7 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 // we pass the object that collects alll the data being passed as props !!
 const ExpenseItem = (props) => {
+  let printclicked1;
   // const classes = "card " + props.className;
   return (
     <>
@@ -13,6 +14,36 @@ const ExpenseItem = (props) => {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button
+          onClick={function () {
+            console.log("clicked!");
+          }}
+        >
+          Change-Title
+        </button>
+
+        {/* All kinds of functions are possible!! */}
+        {/* <button
+          onClick={() => {
+            console.log("clicked!");
+          }}
+        >
+          Change-Title
+        </button>
+        <button
+          onClick={function printclicked() {
+            console.log("clicked!");
+          }}
+        ></button>
+        <button
+          onClick={
+            (printclicked1 = () => {
+              console.log("clicked!");
+            })
+          }
+        >
+          Change-Title
+        </button> */}
       </Card>
     </>
   );
