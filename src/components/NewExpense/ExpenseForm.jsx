d9606f48f02d4eb8b47ab382659d0779306ae5c8
awoliop <awoliioop@gmail.com>
2024-02-17 17:17:17 +0300

    using multiple usestates

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [entereDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const AmountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="">Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label htmlFor="">Amount</label>
+          <label htmlFor="" onChange={AmountChangeHandler}>
+            Amount
+          </label>
           <input type="number" min="0.01" max="0.01" />
         </div>
         <div className="new-expense__control">
-          <label htmlFor="">Date</label>
+          <label htmlFor="" onChange={dateChangeHandler}>
+            Date
+          </label>
           <input type="date" min="2019-01-01" max="2024-02-17" />
         </div>
         <div className="new-expense__actions">
